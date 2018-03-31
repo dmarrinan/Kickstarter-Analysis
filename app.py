e9134b1e393be429e0b71e79947afcabf36cd81a
Dan Marrinan <dan.marrinan@gmail.com>
2018-03-31 15:08:45 -0500

    adds month created to data_engineering.ipynb and kickstarter_campaigns.db. changes how category and parent category dropdowns are populated. gets the flask route for the month started stacked bar chart working.

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from flask import(
 )
 
 # Database Setup
-#engine = create_engine("sqlite:///db/winter_olympics.sqlite")
+engine = create_engine("sqlite:///db/kickstarter_campaigns.sqlite")
 
 # reflect existing database into new model
 Base = automap_base()
@@ -21,19 +21,38 @@ Base = automap_base()
 Base.prepare(engine, reflect=True)
 
 # save reference to tables
-campaigns  = Base.classes.campaigns
+campaigns  = Base.classes.kickstarter_campaigns
 
 # create session to query tables
 session = Session(engine)
 
 app = Flask(__name__)
 
-# create route to return possible category options for select menu
-@app.route('/fetch_categories')
-def fetch_categories():
+# create route to return possible parent category options for select menu
+@app.route('/fetch_parent_categories')
+def fetch_parent_categories():
+    # get possible categories
+    parent_category_list = []
+    response = session.query(distinct(campaigns.parent_category)).all()
+    for category in response:
+        temp, = category
+        parent_category_list.append(temp)
+
+    # return response object
+    return jsonify(parent_category_list)
+
+# create route to return possible category options for select menu for month started stacked bar chart
+@app.route('/fetch_categories_month_started/<parent_category>/')
+def fetch_categories_month_started(parent_category):
     # get possible categories
     category_list = []
-    response = session.query(distinct(campaigns.categories)).all()
+    
+    # filter by parent category
+    queries = []
+    if parent_category != 'All Parent Categories':
+        queries.append(campaigns.parent_category == parent_category)
+
+    response = session.query(distinct(campaigns.category_name)).filter(*queries).all()
     for category in response:
         temp, = category
         category_list.append(temp)
@@ -41,79 +60,93 @@ def fetch_categories():
     # return response object
     return jsonify(category_list)
 
-# create route to return possible subcategory options for select menu
-def fetch_subcategories():
+# create route to return possible category options for select menu for month started stacked bar chart
+@app.route('/fetch_categories_length_campaign/<parent_category>/')
+def fetch_categories_length_campaign(parent_category):
     # get possible categories
-    subcategory_list = []
-    response = session.query(distinct(campaigns.subcategories)).all()
+    category_list = []
+    
+    # filter by parent category
+    queries = []
+    if parent_category != 'All Parent Categories':
+        queries.append(campaigns.parent_category == parent_category)
+
+    response = session.query(distinct(campaigns.category_name)).filter(*queries).all()
     for category in response:
         temp, = category
-        subcategory_list.append(temp)
+        category_list.append(temp)
 
     # return response object
-    return jsonify(subcategory_list)
+    return jsonify(category_list)
 
-@app.route('/month_started_chart/<category>/<subcategory>/')
-def stacked_bar_chart(category, subcategory):
+@app.route('/month_started_chart/<parent_category>/<category>/')
+def stacked_bar_chart(parent_category,category):
     # filter by category
     queries = []
+    if parent_category != 'All Parent Categories':
+        queries.append(campaigns.parent_category == parent_category)
     if category != 'All Categories':
-        queries.append(campaigns.categories == category)
-    if subcategory != 'All Subcategories':
-        queries.append(campaigns.subcategories == subcategory)
-
-    #find month started, length of campaign, category, subcategory, location, successful
-    query = session.query(campaigns.month_started, campaigns.category, campaigns.subcategory)
+        queries.append(campaigns.category_name == category)
 
     queries_successful = queries[:]
-    queries_successful.append(campaigns.successful == 'successful')
+    queries_successful.append(campaigns.state == 'successful')
     queries_failed = queries[:]
-    queries_failed.append(campaigns.successful == 'failed')
+    queries_failed.append(campaigns.state == 'failed')
     queries_canceled = queries[:]
-    queries_canceled.append(campaigns.successful == 'canceled')
+    queries_canceled.append(campaigns.state == 'canceled')
 
     # find number of successful, failed, and canceled campaigns
-    successful_query = session.query(campaigns.month_started, func.count(campaigns.id))
-        .filter(*queries_successful).group_by(campaigns.month_started).all()
-    failed_query = session.query(campaigns.month_started, func.count(campaigns.id))
-        .filter(*queries_failed).group_by(campaigns.month_started).all()
-    canceled_query = session.query(campaigns.month_started, func.count(campaign.id))
-        .filter(*queries_successful).group_by(campaigns.month_started).all()
+    successful_query = session.query(campaigns.month_started, func.count(campaigns.id)).filter(
+        *queries_successful).group_by(campaigns.month_started).all()
+    failed_query = session.query(campaigns.month_started, func.count(campaigns.id)).filter(
+        *queries_failed).group_by(campaigns.month_started).all()
+    canceled_query = session.query(campaigns.month_started, func.count(campaigns.id)).filter(
+        *queries_canceled).group_by(campaigns.month_started).all()
 
     # set up query list for each success state
     #set up return object for successful campaigns
-    num_successful_object = {}
+    num_successful_object = {'num_campaigns':[],'month_name':[]}
     month_list = ['January','February','March','April','May','June','July','August','September','October','November','December']
+    month_object = {'January':0,'February':1,'March':2,'April':3,'May':4,'June':5,'July':6,'August':7,'September':8,'October':9,'November':10,'December':11}
     for month in month_list:
-        num_successful_object[month] = 0
-        num_successful_object['month_name'] = month
+        num_successful_object['num_campaigns'].append(0)
+        num_successful_object['month_name'].append(month)
     # unpack list of tuples for number of campaigns per month
     for row in successful_query:
         month_started, num_campaigns = row
-        num_successful_object[month_started] = num_campaigns
+        num_successful_object['num_campaigns'][month_object[month_started]] = num_campaigns
 
     #set up return object for failed campaigns
-    num_failed_object = {}
+    num_failed_object = {'num_campaigns':[],'month_name':[]}
     for month in month_list:
-        num_failed_object[month] = 0
-        num_successful_object['month_name'] = month
-    # unpack list of tuples for number of medals
+        num_failed_object['num_campaigns'].append(0)
+        num_failed_object['month_name'].append(month)
+    # unpack list of tuples for number of campaigns per month
     for row in failed_query:
         month_started, num_campaigns = row
-        num_failed_object[month_started] = num_campaigns
+        num_failed_object['num_campaigns'][month_object[month_started]] = num_campaigns
 
     #set up return object for canceled campaigns
-    num_canceled_object = {}
+    num_canceled_object = {'num_campaigns':[],'month_name':[]}
     for month in month_list:
-        num_canceled_object[month] = 0
-        num_successful_object['month_name'] = month
-    # unpack list of tuples for number of medals
+        num_canceled_object['num_campaigns'].append(0)
+        num_canceled_object['month_name'].append(month)
+    # unpack list of tuples for number of campaigns per month
     for row in canceled_query:
         month_started, num_campaigns = row
-        num_canceled_object[month_started] = num_campaigns
+        num_canceled_object['num_campaigns'][month_object[month_started]] = num_campaigns
 
    # build response object
     response_object = {'successful': num_successful_object, 'failed': num_failed_object, 'canceled': num_canceled_object}
 
     # return response object
     return jsonify(response_object)
+
+# create route that renders index.html template
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run()
--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,23 +1,61 @@
-//populate select element with possible category values
-categoriesUrl = "/fetch_categories";
-Plotly.d3.json(categoriesUrl, function (error, response) {
+//populate select element with possible parent category values
+parentCategoriesUrl = "/fetch_parent_categories";
+Plotly.d3.json(parentCategoriesUrl, function (error, response) {
     if (error) console.log(error);
-    categoryList = ['All Categories'];
+    parentCategoryList = ['All Parent Categories'];
     for (var i = 0; i < response.length; i++) {
-        categoryList.push(response[i]);
+        parentCategoryList.push(response[i]);
     }
     //console.log(medalList);
-    $categorySelectListMonthStarted = document.getElementById("categorySelectMonthStarted");
-    $categorySelectListLengthCampaign = document.getElementById("categorySelectLengthCampaign");
-    for (var i = 0; i < categoryList.length; i++) {
+    $parentCategorySelectListMonthStarted = document.getElementById("parentCategorySelectMonthStarted");
+    $parentCategorySelectListLengthCampaign = document.getElementById("parentCategorySelectLengthCampaign");
+    for (var i = 0; i < parentCategoryList.length; i++) {
         var $optionMonthStarted = document.createElement("option");
         $optionMonthStarted.value = categoryList[i];
         $optionMonthStarted.text = categoryList[i];
-        $categorySelectListMonthStarted.appendChild($optionMonthStarted);
+        $parentCategorySelectListMonthStarted.appendChild($optionMonthStarted);
 
         var $optionLengthCampaign = document.createElement("option");
         $optionLengthCampaign.value = categoryList[i];
         $optionLengthCampaign.text = categoryList[i];
-        $categorySelectListLengthCampaign.appendChild($optionLengthCampaign);
+        $parentCategorySelectListLengthCampaign.appendChild($optionLengthCampaign);
     }
+
+    //populate select element with possible category values
+    //use parent category to narrow down possible selections
+    parentCategoryMonthStarted = $categorySelectListMonthStarted.value
+    categoriesMonthStartedUrl = `/fetch_categories_month_started/${parentCategoryMonthStarted}`;
+    Plotly.d3.json(categoriesMonthStartedUrl, function (error, response) {
+        if (error) console.log(error);
+        categoryList = ['All Categories'];
+        for (var i = 0; i < response.length; i++) {
+            categoryList.push(response[i]);
+        }
+        //console.log(medalList);
+        $categorySelectListMonthStarted = document.getElementById("categorySelectMonthStarted");
+        for (var i = 0; i < categoryList.length; i++) {
+            var $optionMonthStarted = document.createElement("option");
+            $optionMonthStarted.value = categoryList[i];
+            $optionMonthStarted.text = categoryList[i];
+            $categorySelectListMonthStarted.appendChild($optionMonthStarted);
+        }
+    });
+
+    parentCategoryLengthCampaign = $categorySelectListLengthCampaign.value
+    categoriesLengthCampaignUrl = `/fetch_categories_length_campaign/${parentCategoryLengthCampaign}`;
+    Plotly.d3.json(categoriesLengthCampaignUrl, function (error, response) {
+        if (error) console.log(error);
+        categoryList = ['All Categories'];
+        for (var i = 0; i < response.length; i++) {
+            categoryList.push(response[i]);
+        }
+        //console.log(medalList);
+        $categorySelectListLengthCampaign = document.getElementById("categorySelectLengthCampaign");
+        for (var i = 0; i < categoryList.length; i++) {
+            var $optionLengthCampaign = document.createElement("option");
+            $optionLengthCampaign.value = categoryList[i];
+            $optionLengthCampaign.text = categoryList[i];
+            $categorySelectListLengthCampaign.appendChild($optionLengthCampaign);
+        }
+    });
 });
