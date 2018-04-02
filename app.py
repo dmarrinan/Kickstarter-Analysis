c63f82fc124f63dfcbaaa54cab753c005b1ce983
Dan Marrinan <dan.marrinan@gmail.com>
2018-04-01 22:40:43 -0500

    adds route for country and state maps

--- app.py
+++ app.py
@@ -415,6 +415,277 @@ def category_stacked_bar_chart(month_started,length_campaign,parent_category):
     # return response object
     return jsonify(response_object)
 
+@app.route('/states_map/<month_started>/<length_campaign>/<parent_category>/<category_name>/')
+def states_map(month_started,length_campaign,parent_category,category_name):
+    states_object = {
+        "AL": "Alabama",
+        "AK": "Alaska",
+        "AS": "American Samoa",
+        "AZ": "Arizona",
+        "AR": "Arkansas",
+        "CA": "California",
+        "CO": "Colorado",
+        "CT": "Connecticut",
+        "DE": "Delaware",
+        "DC": "District Of Columbia",
+        "FM": "Federated States Of Micronesia",
+        "FL": "Florida",
+        "GA": "Georgia",
+        "GU": "Guam",
+        "HI": "Hawaii",
+        "ID": "Idaho",
+        "IL": "Illinois",
+        "IN": "Indiana",
+        "IA": "Iowa",
+        "KS": "Kansas",
+        "KY": "Kentucky",
+        "LA": "Louisiana",
+        "ME": "Maine",
+        "MH": "Marshall Islands",
+        "MD": "Maryland",
+        "MA": "Massachusetts",
+        "MI": "Michigan",
+        "MN": "Minnesota",
+        "MS": "Mississippi",
+        "MO": "Missouri",
+        "MT": "Montana",
+        "NE": "Nebraska",
+        "NV": "Nevada",
+        "NH": "New Hampshire",
+        "NJ": "New Jersey",
+        "NM": "New Mexico",
+        "NY": "New York",
+        "NC": "North Carolina",
+        "ND": "North Dakota",
+        "MP": "Northern Mariana Islands",
+        "OH": "Ohio",
+        "OK": "Oklahoma",
+        "OR": "Oregon",
+        "PW": "Palau",
+        "PA": "Pennsylvania",
+        "PR": "Puerto Rico",
+        "RI": "Rhode Island",
+        "SC": "South Carolina",
+        "SD": "South Dakota",
+        "TN": "Tennessee",
+        "TX": "Texas",
+        "UT": "Utah",
+        "VT": "Vermont",
+        "VI": "Virgin Islands",
+        "VA": "Virginia",
+        "WA": "Washington",
+        "WV": "West Virginia",
+        "WI": "Wisconsin",
+        "WY": "Wyoming"
+    }
+    # filter by category
+    queries = [] 
+    queries.append(campaigns.state_or_province.in_(states_object))
+    if month_started != 'All Months':
+        queries.append(campaigns.month_started == month_started)
+
+    if length_campaign == '<=5':
+        queries.append(campaigns.campaign_length <= 5)
+    elif length_campaign == '5-10':
+        queries.append((campaigns.campaign_length > 5) & (campaigns.campaign_length <= 10))
+    elif length_campaign == '10-15':
+        queries.append((campaigns.campaign_length > 10) & (campaigns.campaign_length <= 15)) 
+    elif length_campaign == '15-20':
+        queries.append((campaigns.campaign_length > 15) & (campaigns.campaign_length <= 20))
+    elif length_campaign == '20-25':
+        queries.append((campaigns.campaign_length > 20) & (campaigns.campaign_length <= 25))
+    elif length_campaign == '25-30':
+        queries.append((campaigns.campaign_length > 25) & (campaigns.campaign_length <= 30))
+    elif length_campaign == '30-35':
+        queries.append((campaigns.campaign_length > 30) & (campaigns.campaign_length <= 35))
+    elif length_campaign == '35-40':
+        queries.append((campaigns.campaign_length > 35) & (campaigns.campaign_length <= 40))
+    elif length_campaign == '40-45':
+        queries.append((campaigns.campaign_length > 40) & (campaigns.campaign_length <= 45))
+    elif length_campaign == '45-50':
+        queries.append((campaigns.campaign_length > 45) & (campaigns.campaign_length <= 50))
+    elif length_campaign == '50-55':
+        queries.append((campaigns.campaign_length > 50) & (campaigns.campaign_length <= 55))
+    elif length_campaign == '55-60':
+        queries.append((campaigns.campaign_length > 55) & (campaigns.campaign_length <= 60))
+    elif length_campaign == '>60':
+        queries.append(campaigns.campaign_length >= 60)
+
+    if parent_category != 'All Parent Categories':
+        queries.append(campaigns.parent_category == parent_category)
+    
+    if category_name != 'All Categories':
+        queries.append(campaigns.category_name == category_name)
+
+    queries_successful = queries[:]
+    queries_successful.append(campaigns.state == 'successful')
+    queries_failed = queries[:]
+    queries_failed.append(campaigns.state == 'failed')
+    queries_canceled = queries[:]
+    queries_canceled.append(campaigns.state == 'canceled')
+
+    # find number of successful, failed, and canceled campaigns
+    successful_query = session.query(campaigns.state_or_province, campaigns.country, func.count(campaigns.id)).filter(
+        *queries_successful).group_by(campaigns.state_or_province).all()
+    failed_query = session.query(campaigns.state_or_province, campaigns.country, func.count(campaigns.id)).filter(
+        *queries_failed).group_by(campaigns.state_or_province).all()
+    canceled_query = session.query(campaigns.state_or_province, campaigns.country, func.count(campaigns.id)).filter(
+        *queries_canceled).group_by(campaigns.state_or_province).all()
+
+    state_query = session.query(distinct(campaigns.state_or_province)).filter(*queries).all()
+    state_list = []
+    state_object = {}
+    for index,row in enumerate(state_query):
+        temp, = row
+        state_list.append(temp)
+        state_object[temp] = index
+
+    # set up query list for each success state
+    #set up return object for successful campaigns
+    num_successful_object = {'num_campaigns':[],'state_name':[]}
+
+    for state in state_list:
+        num_successful_object['num_campaigns'].append(0)
+        num_successful_object['state_name'].append(state)
+
+    # unpack list of tuples for number of campaigns per month
+    for row in successful_query:
+        state_name, country_name, num_campaigns = row
+        num_successful_object['num_campaigns'][state_object[state_name]] = num_campaigns
+
+    #set up return object for failed campaigns
+    num_failed_object = {'num_campaigns':[], 'state_name':[]}
+
+    for state in state_list:
+        num_failed_object['num_campaigns'].append(0)
+        num_failed_object['state_name'].append(state)
+
+    # unpack list of tuples for number of campaigns per month
+    for row in failed_query:
+        state_name, country_name, num_campaigns = row
+        num_failed_object['num_campaigns'][state_object[state_name]] = num_campaigns
+
+    #set up return object for canceled campaigns
+    num_canceled_object = {'num_campaigns':[], 'state_name':[]}
+
+    for state in state_list:
+        num_canceled_object['num_campaigns'].append(0)
+        num_canceled_object['state_name'].append(state)
+
+    # unpack list of tuples for number of campaigns per month
+    for row in canceled_query:
+        state_name, country_name, num_campaigns = row
+        num_canceled_object['num_campaigns'][state_object[state_name]] = num_campaigns
+
+   # build response object
+    response_object = {'successful': num_successful_object, 'failed': num_failed_object, 'canceled': num_canceled_object, 'state': state_list}
+
+    # return response object
+    return jsonify(response_object)
+
+@app.route('/countries_map/<month_started>/<length_campaign>/<parent_category>/<category_name>/')
+def countries_map(month_started,length_campaign,parent_category,category_name):
+    # filter by category
+    queries = [] 
+    if month_started != 'All Months':
+        queries.append(campaigns.month_started == month_started)
+
+    if length_campaign == '<=5':
+        queries.append(campaigns.campaign_length <= 5)
+    elif length_campaign == '5-10':
+        queries.append((campaigns.campaign_length > 5) & (campaigns.campaign_length <= 10))
+    elif length_campaign == '10-15':
+        queries.append((campaigns.campaign_length > 10) & (campaigns.campaign_length <= 15)) 
+    elif length_campaign == '15-20':
+        queries.append((campaigns.campaign_length > 15) & (campaigns.campaign_length <= 20))
+    elif length_campaign == '20-25':
+        queries.append((campaigns.campaign_length > 20) & (campaigns.campaign_length <= 25))
+    elif length_campaign == '25-30':
+        queries.append((campaigns.campaign_length > 25) & (campaigns.campaign_length <= 30))
+    elif length_campaign == '30-35':
+        queries.append((campaigns.campaign_length > 30) & (campaigns.campaign_length <= 35))
+    elif length_campaign == '35-40':
+        queries.append((campaigns.campaign_length > 35) & (campaigns.campaign_length <= 40))
+    elif length_campaign == '40-45':
+        queries.append((campaigns.campaign_length > 40) & (campaigns.campaign_length <= 45))
+    elif length_campaign == '45-50':
+        queries.append((campaigns.campaign_length > 45) & (campaigns.campaign_length <= 50))
+    elif length_campaign == '50-55':
+        queries.append((campaigns.campaign_length > 50) & (campaigns.campaign_length <= 55))
+    elif length_campaign == '55-60':
+        queries.append((campaigns.campaign_length > 55) & (campaigns.campaign_length <= 60))
+    elif length_campaign == '>60':
+        queries.append(campaigns.campaign_length >= 60)
+
+    if parent_category != 'All Parent Categories':
+        queries.append(campaigns.parent_category == parent_category)
+    
+    if category_name != 'All Categories':
+        queries.append(campaigns.category_name == category_name)
+
+    queries_successful = queries[:]
+    queries_successful.append(campaigns.state == 'successful')
+    queries_failed = queries[:]
+    queries_failed.append(campaigns.state == 'failed')
+    queries_canceled = queries[:]
+    queries_canceled.append(campaigns.state == 'canceled')
+
+    # find number of successful, failed, and canceled campaigns
+    successful_query = session.query(campaigns.country, func.count(campaigns.id)).filter(
+        *queries_successful).group_by(campaigns.country).all()
+    failed_query = session.query(campaigns.country, func.count(campaigns.id)).filter(
+        *queries_failed).group_by(campaigns.country).all()
+    canceled_query = session.query(campaigns.country, func.count(campaigns.id)).filter(
+        *queries_canceled).group_by(campaigns.country).all()
+
+    country_query = session.query(distinct(campaigns.country)).filter(*queries).all()
+    country_list = []
+    country_object = {}
+    for index,row in enumerate(country_query):
+        temp, = row
+        country_list.append(temp)
+        country_object[temp] = index
+
+    # set up query list for each success state
+    #set up return object for successful campaigns
+    num_successful_object = {'num_campaigns':[],'country_name':[]}
+
+    for country in country_list:
+        num_successful_object['num_campaigns'].append(0)
+        num_successful_object['country_name'].append(country)
+    # unpack list of tuples for number of campaigns per month
+    for row in successful_query:
+        country_name, num_campaigns = row
+        num_successful_object['num_campaigns'][country_object[country_name]] = num_campaigns
+
+    #set up return object for failed campaigns
+    num_failed_object = {'num_campaigns':[],'country_name':[]}
+
+    for country in country_list:
+        num_failed_object['num_campaigns'].append(0)
+        num_failed_object['country_name'].append(country)
+    # unpack list of tuples for number of campaigns per month
+    for row in failed_query:
+        country_name, num_campaigns = row
+        num_failed_object['num_campaigns'][country_object[country_name]] = num_campaigns
+
+    #set up return object for canceled campaigns
+    num_canceled_object = {'num_campaigns':[],'country_name':[]}
+
+    for country in country_list:
+        num_canceled_object['num_campaigns'].append(0)
+        num_canceled_object['country_name'].append(country)
+    # unpack list of tuples for number of campaigns per month
+    for row in canceled_query:
+        country_name, num_campaigns = row
+        num_canceled_object['num_campaigns'][country_object[country_name]] = num_campaigns
+
+   # build response object
+    response_object = {'successful': num_successful_object, 'failed': num_failed_object, 'canceled': num_canceled_object}
+
+    # return response object
+    return jsonify(response_object)
+
 # create route that renders index.html template
 @app.route("/")
 def home():
